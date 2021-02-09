560cbfbf5269428d5d02bc2bc015ba8afa0c6a46
Eli Pacheco <eli.pacheco55@gmail.com>
2021-02-09 12:29:29 -0700

    working changes

---
+++ print_students.py
@@ -0,0 +1,8 @@
+from define_student import Student 
+
+student1 = Student("Jim", "Business" ,3.1, False)
+student2 = Student("Pam", "Art", 2.5, True)
+student3 = Student("Randy", "CS", 3.8, False)
+print(student1.name)
+print(student2.name)
+print(student3.name)
