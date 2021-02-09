e9262f48cf6d15189fc3cf3db3e1738b5e978734
Eli Pacheco <eli.pacheco55@gmail.com>
2021-02-09 12:40:50 -0700

    Updated students

---
+++ define_student.py
@@ -0,0 +1,8 @@
+
+
+class Student:  # Class to define a student 
+    def __init__ (self, name, major, gpa, is_on_probation):
+        self.name = name # name of the student
+        self.major = major # their major
+        self.gpa = gpa # grade point average 
+        self.is_on_probation = is_on_probation #boolean, are they on probation?
